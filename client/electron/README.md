1b5c654ae7dbb9339d099d7af97445bdbe95f694
Steven BOEHM <boehm_s@etna-alternance.net>
2017-10-11 20:41:39 +0200

    messed up with submodules

---
+++ client/electron/README.md
@@ -0,0 +1,5 @@
+# Softwar
+
+This app is based on electron-quick-start
+Be sure to install zeromq with your local version of the lib (3.2).
+Do not forget to rebuild zeromq for electron : `npm rebuild zeromq --runtime=electron --target=1.7.9`
---
+++ client/electron/renderer.js
@@ -0,0 +1,23 @@
+// This file is required by the index.html file and will
+// be executed in the renderer process for that window.
+// All of the Node.js APIs are available in this process.
+
+const zmq = require('zeromq');
+const req_socket = zmq.socket('req');
+const sub_socket = zmq.socket('sub');
+
+const genID = () => {
+    const n = Math.floor(Math.random() * (99 - 0));
+    const str = n.toString();
+    return (2 !== str.length)
+	? '#0x0' + str
+	: '#0x' + str;
+};
+
+req_socket.connect('tcp://127.0.0.1:4243');
+req_socket.identity = genID();
+sub_socket.connect('tcp://127.0.0.1:4242');
+sub_socket.subscribe('');
+
+
+module.exports = {req_socket, sub_socket};
